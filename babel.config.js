7256517c4c81aa9a83c7ccd5ac10b3a8bf7d6acb
Daniel Papilion <papiliond.94@gmail.com>
2024-09-11 11:32:22 +0200

    Add @babel/plugin-transform-nullish-coalescing-operator v2

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
 /* eslint-disable unicorn/prefer-module */
 module.exports = {
+  plugins: [
+    '@babel/plugin-transform-nullish-coalescing-operator',
+  ],
   env: {
     test: {
       presets: [
