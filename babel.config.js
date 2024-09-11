2e6f93d2c23a4dd2cd0495d325254b9d727e2210
Daniel Papilion <papiliond.94@gmail.com>
2024-09-11 13:56:26 +0200

    Add babel preset

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 /* eslint-disable unicorn/prefer-module */
 module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
   env: {
     test: {
       presets: [
